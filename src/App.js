e94284aa95e03413aa31f035e4429bbd1e186f24
Lina Ramirez <Lina.Ramirez@jala.university>
2023-02-10 19:30:35 -0400

    Add pokedex

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
+
+  const [currentId, setcurrentId] = useState()
+  const [pokemon, setPokemon] = useState({});
+  const pokeApe = `https://pokeapi.co/api/v2/pokemon/`;
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(()=> {
+    fetch(`${pokeApe}${currentId}`)
+    .then(response => response.json())
+    .then(pokemonData => {
+      console.log(pokemonData);
+      setcurrentId(pokemonData.id)
+      setPokemon(pokemonData);
+    })
+  }, [currentId]);
+
+  const getPokemon = (id) => {
+    setcurrentId(id);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {
+        isLoading ? (
+          <></>
+        ) : (
+          <div>
+            <div>
+              <label>{pokemon.name}</label> 
+            </div> 
+            <div> 
+              <img src={pokemon.sprites.front_default} className="App-logo" alt="logo" />
+            </div> 
+            <div>
+              <button onClick={()=> getPokemon(currentId + 1)}>Next</button>
+            </div>
+          </div>
+        )
+      }
       </header>
-    </div>
+    </div> 
   );
 }
 
