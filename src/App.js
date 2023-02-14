ef015e52651043ded46d99cdfdf3f34885ac9deb
Lina Ramirez <Lina.Ramirez@jala.university>
2023-02-14 11:45:46 -0400

    Add abilities

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
 
-  const [currentId, setcurrentId] = useState()
-  const [pokemon, setPokemon] = useState({});
+  const [currentId, setcurrentId] = useState(1)
+  const [pokemon, setPokemon] = useState({sprites:{}, weight:0, abilities:[]});
   const pokeApe = `https://pokeapi.co/api/v2/pokemon/`;
   const [isLoading, setIsLoading] = useState(false);
 
@@ -26,10 +26,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      {
-        isLoading ? (
-          <></>
-        ) : (
+      
           <div>
             <div>
               <label>{pokemon.name}</label> 
@@ -38,17 +35,29 @@ function App() {
               <img src={pokemon.sprites.front_default} className="App-logo" alt="logo" />
             </div> 
             <div>
+              <label>WEIGHT: </label> 
+              <br/>
+              <label>{pokemon.weight}</label> 
+            </div>
+            <div>
+              <br/>
+              <label>ABILITIES: </label> 
+              <label>{pokemon.abilities.map(item => (
+                <div key={uuidv4()}>
+                  <lable>{item.ability.name}</lable>
+                </div>
+              ))}</label> 
+            </div>
+            <div>
+              <br/>
               <button onClick={()=> getPokemon(currentId + 1)}>Next</button>
             </div>
             <div>
               <button onClick={()=> getPokemon(currentId - 1)}>Previus</button>
             </div>
-            <div>
-              <label>{pokemon.weigth}</label>
-            </div>
           </div>
-        )
-      }
+        
+      
       </header>
     </div> 
   );
