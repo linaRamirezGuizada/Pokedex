971b7cbd65a7e0b53ac2265001e5790bd89d6aaf
Lina Ramirez <Lina.Ramirez@jala.university>
2023-02-14 16:19:37 -0400

    Add first part weaknesses

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import {useEffect, useState} from 'react';
 import {v4 as uuidv4} from 'uuid';
 
 function App() {
+  
+  const Weaknesses =  [
+    {
+      type: "grass",
+      weaknesses: [
+                    "Flying", 
+                    "Fire", 
+                    "Ice", 
+                    "Poison"
+                  ]
+    },
+    {
+      type: "poison",
+      weaknesses: [
+                    "Psychic"
+                  ]
+    }
+  ];
 
   const [currentId, setcurrentId] = useState(1)
-  const [pokemon, setPokemon] = useState({sprites:{}, weight:0, abilities:[]});
+  const [pokemon, setPokemon] = useState({sprites:{}, weight:0, abilities:[], Weaknesses:[]});
   const pokeApe = `https://pokeapi.co/api/v2/pokemon/`;
   const [isLoading, setIsLoading] = useState(false);
 
@@ -26,7 +44,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+      {
+          isLoading ? (
+            <></>
+          ) : (
+
           <div>
             <div>
               <label>{pokemon.name}</label> 
@@ -50,14 +72,23 @@ function App() {
             </div>
             <div>
               <br/>
+              <label>WEAKNESSES: </label>
+              {/* <label>{pokemon.Weaknesses.map(item => (
+                <div key={uuidv4()}>
+                  <lable>{item.weaknesses.type}</lable>
+                </div>
+              ))}</label>    */}
+            </div>            
+            <div>
+              <br/>
               <button onClick={()=> getPokemon(currentId + 1)}>Next</button>
             </div>
             <div>
               <button onClick={()=> getPokemon(currentId - 1)}>Previus</button>
             </div>
           </div>
-        
-      
+        )
+      }        
       </header>
     </div> 
   );
